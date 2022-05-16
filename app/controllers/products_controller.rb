14e1f5624d31a35b1bcb94b82cd74a7be21e127b
Philippe De Andria <philippedeandria12@hotmail.com>
2022-05-16 21:33:17 +0300

    all crud actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,9 +24,15 @@ class ProductsController < ApplicationController
   end
 
   def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    redirect_to product_path(@product)
   end
 
   def destroy
+    @product = Product.find(params[:id)
+    @product.destroy
+    redirect_to products_path
   end
 
   private
