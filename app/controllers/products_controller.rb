88561d5f4e673c0de75db5fc49e2398dd479aa1b
Philippe De Andria <philippedeandria12@hotmail.com>
2022-05-16 21:30:36 +0300

    new , show and index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,18 +9,30 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
   end
 
   def create
+    # raise // Whenever you add a new product, if you raise and do params[:product], you'll see all params.
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to product_path(@product)
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :rating, :category, :price)
   end
 
 end
